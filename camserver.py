896465eed4e1c6a5048693c063a0ca7c73dc7327
zombiecrab <kingofthemods@gmail.com>
2024-07-18 16:41:25 +0100

    Update web control for project usecase.

--- camserver.py
+++ camserver.py
@@ -5,7 +5,7 @@ from picamera2.outputs import FileOutput, CircularOutput
 import io
 
 import subprocess
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from flask_restful import Resource, Api, reqparse, abort
 import atexit
 from datetime import datetime
@@ -24,7 +24,7 @@ output = CircularOutput()
 class Camera:
     def __init__(self):
         self.camera = picamera2.Picamera2()
-        self.camera.configure(self.camera.create_video_configuration(main={"size": (640, 480)}))
+        self.camera.configure(self.camera.create_video_configuration(main={"size": (800, 600)}))
         self.still_config = self.camera.create_still_configuration()
         self.encoder = MJPEGEncoder(10000000)
         self.streamOut = StreamingOutput()
@@ -40,16 +40,6 @@ class Camera:
             self.streamOut.condition.wait()
             self.frame = self.streamOut.frame
         return self.frame
-    
-    def VideoSnap(self):
-        print("Snap")
-        timestamp = datetime.now().isoformat("_","seconds")
-        print(timestamp)
-        self.still_config = self.camera.create_still_configuration()
-        self.file_output = "/home/allzero22/Webserver/webcam/static/pictures/snap_%s.jpg" % timestamp
-        time.sleep(1)
-        self.job = self.camera.switch_mode_and_capture_file(self.still_config, self.file_output, wait=False)
-        self.metadata = self.camera.wait(self.job)
     
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -78,15 +68,6 @@ class VideoFeed(Resource):
         return Response(genFrames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
     
-# Timestamp
-def show_time():
-    ''' Show current date time in text format '''
-    rightNow = datetime.now()
-    print(rightNow)
-    currentTime = rightNow.strftime("%d-%m-%Y_%H:%M:%S")
-    print("date and time =", currentTime)
-        
-    return currentTime
 
 @app.route('/')
 def index():
@@ -94,55 +75,12 @@ def index():
     
     return render_template('index.html')
 
-@app.route('/home', methods = ['GET', 'POST'])
-def home_func():
-    """Video streaming home page."""
-    
-    return render_template("index.html")
-
-@app.route('/info.html')
-def info():
-    """Info Pane"""
-    
-    return render_template('info.html')
-
-@app.route('/startRec.html')
-def startRec():
-    """Start Recording Pane"""
-    print("Video Record")
-    basename = show_time()
-    directory = basename
-    parent_dir = "/home/allzero22/Webserver/webcam/static/video/"
-    output.fileoutput = (parent_dir + "Bird_%s.h264"  %directory)
-    output.start()
-    
-    return render_template('startRec.html')
-
-@app.route('/stopRec.html')
-def stopRec():
-    """Stop Recording Pane"""
-    print("Video Stop")
-    output.stop()
-    
-    return render_template('stopRec.html')
-
-@app.route('/srecord.html')
-def srecord():
-    """Sound Record Pane"""
-    print("Recording Sound")
-    timestamp = datetime.now().isoformat("_","seconds")
-    print(timestamp)
-    subprocess.Popen('arecord -D dmic_sv -d 30 -f S32_LE /home/allzero22/Webserver/webcam/static/sound/birdcam_$(date "+%b-%d-%y-%I:%M:%S-%p").wav -c 2', shell=True)
-    
-    return render_template('srecord.html')
-
-@app.route('/snap.html')
-def snap():
-    """Snap Pane"""
-    print("Taking a photo")
-    camera.VideoSnap()
-    
-    return render_template('snap.html')
+@app.route('/submission', methods= ['POST', 'GET'])
+def form_return():
+    if request.method == 'POST':
+        run_time = request.form['runTime']
+        print(run_time)
+    return render_template('index.html')
 
 api.add_resource(VideoFeed, '/cam')
 
